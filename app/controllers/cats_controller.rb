9450fbeb2fa0448e154affbc3de4dada941bc2fd
chxuri <rmamedova2010@gmail.com>
2025-12-27 19:26:52 -0600

    added the U in CRUD

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,4 +6,35 @@ class CatsController < ApplicationController
   def show
     @cat = Cat.find(params[:id])
   end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to @cat
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to @cat
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def cat_params
+      params.expect(cat: [ :name ])
+    end
 end
